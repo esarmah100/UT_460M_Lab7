cd439eb59954ea646784db7945b09ec68a59da67
JaxonCoward <jaxon@DESKTOP-GSOR9ME.localdomain>
2021-05-01 16:28:17 -0500

    Modified rotate

--- rotate.asm
+++ rotate.asm
@@ -1,11 +1,11 @@
 start:
   andi  $2, $2, 0
   addi  $2, $2, 128
+rot
+  andi  $1, $1, 0
+  addi  $1, $1, 1
 loop:
   beq   $1, $2, rot
   sll   $1, $1, 1
   j     loop
-rot:
-  sll   $1, $1, 1
-  addi  $1, $1, 1
-  j     loop
+  
